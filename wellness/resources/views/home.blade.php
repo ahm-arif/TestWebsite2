b2a85814b88551871dd5505dc4b6a65278b5e8d6
andreas <39374691+caren55@users.noreply.github.com>
2020-09-13 11:52:53 +0700

    changes

---
+++ wellness/resources/views/home.blade.php
@@ -0,0 +1,194 @@
+@extends('index')
+@section('title','Wellness Center || Home')
+@section('content')
+   <!--::banner part start::-->
+   <section class="banner_part">
+      <div class="container">
+         <div class="row align-content-center">
+            <div class="col-lg-6">
+               <div class="banner_text aling-items-center">
+                  <div class="banner_text_iner">
+                     <h5>Healthcare consultant</h5>
+                     <h2>Wellness Center, <br>The High-quality healthcare platform made for you</h2>
+                     <p>Wellness Center adalah sebuah aplikasi kesehatan yang memungkinkan dokter datang ke rumah anda untuk melakukan medical check-up dan melakukan konsultasi online. Kami juga menyediakan obat-obatan yang dapat dibeli secara online </p>
+                     <a href="#" class="btn_1 banner_btn ">View Property</a>
+                     <div class="d-none d-xl-block banner_social_icon">
+                        <ul class="list-inline">
+
+                           <li class="list-inline-item"><a href="#"><span class="ti-facebook"></span>facebook</a><span
+                                 class="dot"><i class="fas fa-circle"></i></span></li>
+                           <li class="list-inline-item"><a href="#"><span class="ti-twitter-alt"></span>twitter</a><span
+                                 class="dot"><i class="fas fa-circle"></i></span></li>
+                           <li class="list-inline-item"><a href="#"><span class="ti-instagram"></span>instagram</a></li>
+                        </ul>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <a href="https://www.youtube.com/watch?v=pBFQdxA-apI" class="popup-youtube video_popup"><span
+            class="ti-control-play"></span></a>
+
+   </section>
+   <!--::banner part end::-->
+
+   <!--::team part end::-->
+   <section class="team_part">
+      <div class="container">
+         <div class="row">
+            <div class="col-lg-6">
+               <div class="team_img"></div>
+            </div>
+            <div class="offset-lg-1 col-lg-5">
+               <div class="team_member_text">
+                  <h2>Living From <br>
+                     The Team That Cares</h2>
+                  <p>Unto all set life creeping own set. Saw make multiply female watge deb all set life
+                     creeping own set. Saw make multiply female watge abund winged subdue dominion
+                     own night days second</p>
+                  <ul>
+                     <li><span class="ti-paint-bucket"></span>Set dry signs spirit a kind First shall them windged
+                        creping</li>
+                     <li><span class="ti-check-box"></span>He two face one moved dominion man you're likeness</li>
+                     <li><span class="ti-ruler-pencil"></span>Sea forth fill have divide be dominion from life for feven
+                     </li>
+                  </ul>
+                  <a href="#" class="btn_1">Read More</a>
+               </div>
+            </div>
+         </div>
+      </div>
+
+   </section>
+   <!--::team part end::-->
+
+   <!--::passion_part end::-->
+   <div class="passion_part">
+      <div class="container">
+         <div class="row">
+            <div class="col-lg-5">
+               <div class="section_tittle">
+                  <h1>Our Features</h1>
+               </div>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-md-6 col-lg-3">
+               <div class="single_passion">
+                  <div class="single_passion_item">
+                     <a href="#" class="passion_icon"> <img src="img/chat.png">></i> </a>
+                     <h4>Konsultasi Online</h4>
+                     <p>Hac facilisi ac vitae consec tetu commod vel magna suspen disse on senectus
+                        pharetra magnfauc bed</p>
+                     <a href="#" class="btn_2">Read More <span class="ti-arrow-right"></span></a>
+                  </div>
+               </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+               <div class="single_passion">
+                  <div class="single_passion_item">
+                     <a href="#" class="passion_icon"> <img src="img/vidcall.png"></i> </a>
+                     <h4>Video Call</h4>
+                     <p>Hac facilisi ac vitae consec tetu commod vel magna suspen disse on senectus
+                        pharetra magnfauc bed</p>
+                     <a href="#" class="btn_2">Read More <span class="ti-arrow-right"></span></a>
+                  </div>
+               </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+               <div class="single_passion">
+                  <div class="single_passion_item">
+                     <a href="#" class="passion_icon"> <img src="img/door.png"></i> </a>
+                     <h4>Door to Door Service</h4>
+                     <p>Hac facilisi ac vitae consec tetu commod vel magna suspen disse on senectus
+                        pharetra magnfauc bed</p>
+                     <a href="#" class="btn_2">Read More <span class="ti-arrow-right"></span></a>
+                  </div>
+               </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+               <div class="single_passion">
+                  <div class="single_passion_item">
+                     <a href="#" class="passion_icon"> <img src="img/medicine.png"></i> </a>
+                     <h4>Obat-obatan</h4>
+                     <p>Hac facilisi ac vitae consec tetu commod vel magna suspen disse on senectus
+                        pharetra magnfauc bed</p>
+                     <a href="#" class="btn_2">Read More <span class="ti-arrow-right"></span></a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+   <!--::passion_part end::-->
+
+
+
+   <!--::review_part end::-->
+   <div class="review_part" id="testimonial">
+      <div class="container">
+         <div class="row">
+            <div class="col-sm-8 col-lg-7">
+               <div class="section_tittle">
+                  <h1>Kata mereka</h1>
+               </div>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-lg-12">
+               <div class="review_part_text owl-carousel">
+                  <div class="singler_eview_part">
+                     <div class="client_info">
+                        <img src="img/client_1.png" alt="">
+                        <h4>Mouunt Alezaber</h4>
+                        <p>54 Tahun, Ibu Rumah Tangga</p>
+                     </div>
+                     <p><i>Wellness Center sangat berbeda dengan aplikasi kesehatan lainnya. Dengan tambahan video call yang tersedia membuat konsultasi menjadi lebih mudah, Terimakasih Wellness center !</i></p>
+                  </div>
+                  <div class="singler_eview_part">
+                     <div class="client_info">
+                        <img src="img/client_2.png" alt="">
+                        <h4>Mouunt Alezaber</h4>
+                        <p>Project manager</p>
+                     </div>
+                     <p><i>Enim a, scelerisque aliquet. Vivamus neque diam sed at, pede laoreet orci.
+                           Potenti vel In sagittis nulla augue vitae et tempus torquent. Lacinia neque
+                           mus taciti ante prsent at facilisis. Enim a, scelerisque aliquet. Vivamus
+                           neque diam sed at, pede laoreet orci Potti, vel. In sagittis nulla augue
+                           vitae et tempus torquent.</i></p>
+                  </div>
+                  <div class="singler_eview_part">
+                     <div class="client_info">
+                        <img src="img/client_1.png" alt="">
+                        <h4>Mouunt Alezaber</h4>
+                        <p>Project manager</p>
+                     </div>
+                     <p><i>Enim a, scelerisque aliquet. Vivamus neque diam sed at, pede laoreet orci.
+                           Potenti vel In sagittis nulla augue vitae et tempus torquent. Lacinia neque
+                           mus taciti ante prsent at facilisis. Enim a, scelerisque aliquet. Vivamus
+                           neque diam sed at, pede laoreet orci Potti, vel. In sagittis nulla augue
+                           vitae et tempus torquent.</i></p>
+                  </div>
+                  <div class="singler_eview_part">
+                     <div class="client_info">
+                        <img src="img/client_2.png" alt="">
+                        <h4>Mouunt Alezaber</h4>
+                        <p>Project manager</p>
+                     </div>
+                     <p><i>Enim a, scelerisque aliquet. Vivamus neque diam sed at, pede laoreet orci.
+                           Potenti vel In sagittis nulla augue vitae et tempus torquent. Lacinia neque
+                           mus taciti ante prsent at facilisis. Enim a, scelerisque aliquet. Vivamus
+                           neque diam sed at, pede laoreet orci Potti, vel. In sagittis nulla augue
+                           vitae et tempus torquent.</i></p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+   <!--::review_part end::-->
+
+
+
+@stop
